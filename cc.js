dd4fa8bd9e47cab1d91a6d6081c4cf07f7a4d655
KarmoAW <frozyice@gmail.com>
2023-09-18 15:50:21 +0300

    add simple cookie click

--- cc.js
+++ cc.js
@@ -1,1 +1,8 @@
-alert('yello?');
+let bigCookie = document.getElementById('bigCookie');
+
+do {
+    bigCookie.click();
+    setTimeout(() => {
+          bigCookie.click();
+        }, 100);
+} while (true)
